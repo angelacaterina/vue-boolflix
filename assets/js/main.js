d0739aac9c437adfaee20cafe3d5e6f97c39d0eb
angelacaterina <angela.caterina@icloud.com>
2021-01-14 19:39:16 +0100

    Milestone 4 finita
    servirebbero solo dei piccoli abbellimenti nel css, è stato creato un unico main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-// Milestone 3:
-//1. In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/​ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400​) per poi aggiungere la parte finale dell’URL passata dall’API.
-// Esempio di URL: https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
 let app = new Vue({
   el: '#app',
   data: {
